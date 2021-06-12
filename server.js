4277d8574c5bd684312910aa237f786580f8b99e
brownnicholasj <brown.nicholasj@gmail.com>
2021-06-12 10:49:48 -0500

    insert handlebars helper for date

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
-
-const hbs = exphbs.create({});
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
