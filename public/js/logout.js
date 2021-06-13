fe14efb6a3adb0d0d3df756b85e632ca4582937e
brownnicholasj <brown.nicholasj@gmail.com>
2021-06-13 18:50:42 -0500

    login and logout working -- logout viewer still not fully functional

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,48 +1,13 @@
 const logoutHandler = async (event) => {
 	event.preventDefault();
 
-	const email = document.querySelector('#email-login').value.trim();
-	const password = document.querySelector('#password-login').value.trim();
+	const response = await fetch('/api/login/logout', {
+		method: 'POST',
+		body: JSON.stringify(),
+		headers: { 'Content-Type': 'application/json' },
+	});
 
-	if (email && password) {
-		const response = await fetch('/api/users/login', {
-			method: 'POST',
-			body: JSON.stringify({ email, password }),
-			headers: { 'Content-Type': 'application/json' },
-		});
-
-		if (response.ok) {
-			document.location.replace('/');
-		} else {
-			alert('Failed to log in.');
-		}
-	}
-};
-
-const signupFormHandler = async (event) => {
-	event.preventDefault();
-
-	const username = document.querySelector('#username-signup').value.trim();
-	const email = document.querySelector('#email-signup').value.trim();
-	const password = document.querySelector('#password-signup').value.trim();
-
-	if (username && email && password) {
-		const response = await fetch('/api/login/signin', {
-			method: 'POST',
-			body: JSON.stringify({ username, email, password }),
-			headers: { 'Content-Type': 'application/json' },
-		});
-
-		if (response.ok) {
-			document.location.replace('/');
-		} else {
-			alert('Failed to sign up.');
-		}
-	}
+	document.location.replace('/');
 };
 
 document.querySelector('.logout').addEventListener('click', logoutHandler);
-
-document
-	.querySelector('.signup-form')
-	.addEventListener('submit', signupFormHandler);
